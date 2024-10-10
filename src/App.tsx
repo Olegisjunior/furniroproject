135bd73a945f71c0f18ae9b2c814206faf12f930
Olegisjunior <olegzavarinskiy@icloud.com>
2024-10-10 10:14:52 +0300

    bag fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { HomePage } from "./Pages/HomePage";
 import { FooterSection } from "./Pages/FooterSection";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import React from "react";
+import React, { Suspense } from "react";
+import { Oval } from "react-loader-spinner";
 
 const ShopPage = React.lazy(() => import("./Pages/ShopPage"));
 const AboutPage = React.lazy(() => import("./Pages/AboutPage"));
@@ -34,20 +35,37 @@ export const App = () => {
         theme="colored"
       />
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/furniture/:productId" element={<ItemPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/compare" element={<Compare />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/dining" element={<DiningPage />} />
-        <Route path="/living" element={<LivingPage />} />
-        <Route path="/bedroom" element={<BedroomPage />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center w-full h-full left-0 right-0 top-0 bottom-0 mx-auto">
+            <Oval
+              visible={true}
+              height="80"
+              width="80"
+              color="#1e1e1e"
+              secondaryColor="#797979"
+              ariaLabel="oval-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/furniture/:productId" element={<ItemPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/compare" element={<Compare />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/dining" element={<DiningPage />} />
+          <Route path="/living" element={<LivingPage />} />
+          <Route path="/bedroom" element={<BedroomPage />} />
+        </Routes>
+      </Suspense>
       <FooterSection />
     </>
   );
